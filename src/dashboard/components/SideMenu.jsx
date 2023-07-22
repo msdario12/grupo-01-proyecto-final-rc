c8eb5149d507e70fb368e5ec48b7628c07409cde
msdario12 <mansilladario94@gmail.com>
2023-07-21 22:10:19 -0300

    Se agregan Links de react-router en el sideBar, que redirigen a las respectivas paginas

--- src/dashboard/components/SideMenu.jsx
+++ src/dashboard/components/SideMenu.jsx
@@ -5,6 +5,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const setActiveItem = (state, name) => {
 	return state === name ? 'active' : 'text-white';
@@ -15,45 +16,46 @@ export const SideMenu = () => {
 	return (
 		<div className='d-flex flex-column flex-shrink-0 p-3 text-white bg-dark w-25'>
 			{/* El primero deberia redirigir al home de inicio o el general */}
-			<a
+			<Link
 				className='d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none'
-				href='#'>
+				to='/dashboard'>
 				<span className='fs-4'>RollingVet</span>
-			</a>
+			</Link>
+
 			<hr></hr>
 			<ul className='nav nav-pills flex-column mb-auto'>
 				<li className='nav-item' onClick={() => setSetselectItem('home')}>
-					<a
-						href='#'
+					<Link
+						to={'/dashboard'}
 						className={`nav-link d-flex gap-2 align-items-center ${setActiveItem(
 							selectItem,
 							'home'
 						)}`}>
 						<FontAwesomeIcon icon={faHouse} />
 						Inicio
-					</a>
+					</Link>
 				</li>
 				<li className='nav-item' onClick={() => setSetselectItem('patients')}>
-					<a
-						href='#'
+					<Link
+						to={'patients'}
 						className={`nav-link d-flex gap-2 align-items-center ${setActiveItem(
 							selectItem,
 							'patients'
 						)}`}>
 						<FontAwesomeIcon icon={faDog} />
 						Pacientes
-					</a>
+					</Link>
 				</li>
 				<li className='nav-item' onClick={() => setSetselectItem('turns')}>
-					<a
-						href='#'
+					<Link
+						to={'turns'}
 						className={`nav-link d-flex gap-2 align-items-center ${setActiveItem(
 							selectItem,
 							'turns'
 						)}`}>
 						<FontAwesomeIcon icon={faCalendarDay} />
 						Turnos
-					</a>
+					</Link>
 				</li>
 			</ul>
 		</div>
