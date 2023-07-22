07df72a873c388e68238988c9dfcde5b4096c995
msdario12 <mansilladario94@gmail.com>
2023-07-21 21:05:10 -0300

    Se crea el componente para el SideMenu en el Dashboard

--- src/dashboard/pages/MainDashboard.jsx
+++ src/dashboard/pages/MainDashboard.jsx
@@ -1,8 +1,11 @@
+import { SideMenu } from '../components/SideMenu';
+
 export const MainDashboard = () => {
 	return (
 		<div>
 			<h2>Esto es MainDashboard</h2>
 			<h3>Subtitulo</h3>
+			<SideMenu />
 		</div>
 	);
 };
