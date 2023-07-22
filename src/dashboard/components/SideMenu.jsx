91626023385a84eba91e585687ccebcb5d5f9c0c
msdario12 <mansilladario94@gmail.com>
2023-07-21 21:25:16 -0300

    Instalacion de fontawesome para react

--- src/dashboard/components/SideMenu.jsx
+++ src/dashboard/components/SideMenu.jsx
@@ -1,7 +1,11 @@
-
-
 export const SideMenu = () => {
 	return (
-		
+		<div className='d-flex flex-column flex-shrink-0 p-3 text-white bg-dark w-25'>
+			<p>RollingVet</p>
+			<hr></hr>
+			<ul className='nav nav-pills flex-column mb-auto'>
+				<li className='nav-item'>Hola</li>
+			</ul>
+		</div>
 	);
 };
