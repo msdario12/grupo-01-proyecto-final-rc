92984e76296dae9436114c7f9340158ef9271086
msdario12 <mansilladario94@gmail.com>
2023-07-25 15:48:59 -0300

    Se agrega diseño inicial del navbar

--- src/ui/components/MainNavBar.jsx
+++ src/ui/components/MainNavBar.jsx
@@ -1,8 +1,36 @@
+import { useState } from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 export const MainNavBar = () => {
+	const [isUserLogged, setIsUserLogged] = useState(false);
 	return (
-		<div>
-			<h2>MainNavBar</h2>
-		</div>
+		<Navbar
+			expand='lg'
+			sticky='top'
+			className='bg-body-tertiary'
+			data-bs-theme='dark'>
+			<Container>
+				<Navbar.Brand href='#home'>
+					<span className='text-info fw-bold'>RollingVet</span>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls='basic-navbar-nav' />
+				<Navbar.Collapse id='basic-navbar-nav'>
+					<Nav className='me-auto'>
+						<Nav.Link href='#link'>Nuestros Planes</Nav.Link>
+						<Nav.Link href='#link2'>Contáctenos</Nav.Link>
+						<Nav.Link href='#link3'>Sobre Nosotros</Nav.Link>
+					</Nav>
+					<Navbar.Text>
+						{isUserLogged ? (
+							<>
+								Signed in as: <a href='#login'>Mark Otto</a>
+							</>
+						) : (
+							<Nav.Link href='#link3'>Acceder</Nav.Link>
+						)}
+					</Navbar.Text>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
 	);
 };
