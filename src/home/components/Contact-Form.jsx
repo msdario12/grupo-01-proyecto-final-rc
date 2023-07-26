0a79e1b5fee3defc687751582b5c0c07ef1221be
Homet Matias <mhometherrera@gmail.com>
2023-07-26 15:08:05 -0300

    Terminada la informacion de contactos

---
+++ src/home/components/Contact-Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const ContactForm = () => {
+  return (
+    <>
+    
+        <div className='contact-form-container mt-5'>
+            <h2>Obten tu consulta online</h2>
+            
+        </div>
+    
+    </>
+  )
+}
--- src/home/pages/ContactPage.jsx
+++ src/home/pages/ContactPage.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { ContactInfo } from '../components/Contact-Info'
+import '../components/Style-Contact.css'
+import { ContactForm } from '../components/Contact-Form'
 
 export const ContactPage = () => {
   return (
     <>
-    
-        <h1>Contact Page</h1>
 
-        <ContactInfo />
+      <h1>Contact Page</h1>
+      <div className="row container-contact-prim">
+        <div className='col-12 col-md-6'>
+          <ContactInfo />
+        </div>
+        <div className='col-12 col-md-6'>
+          <ContactForm />
+        </div>
+      </div>
     </>
   )
 }
