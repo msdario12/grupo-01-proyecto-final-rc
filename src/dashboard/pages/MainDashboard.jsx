25997b30b9aa6ee3fcb7812d97d20ffe5df258ce
msdario12 <mansilladario94@gmail.com>
2023-07-26 17:57:52 -0300

    se vuelve las cosas al commit antes del pull

--- src/dashboard/pages/MainDashboard.jsx
+++ src/dashboard/pages/MainDashboard.jsx
@@ -1,21 +1,9 @@
-
-import { Outlet } from 'react-router-dom';
-import { SideMenu } from '../components/SideMenu';
-import { MainNavBar } from '../../ui/components/MainNavBar';
+import { WelcomeInfo } from '../components/WelcomeInfo';
 
 export const MainDashboard = () => {
 	return (
-		<main className='container-fluid ps-0 '>
-			<div className='row'>
-				<div className='col-4 min-vh-100 pe-0'>
-					<SideMenu />
-				</div>
-				<div className='col-8 px-0'>
-					<MainNavBar isInDashboard={true} />
-					<Outlet />
-				</div>
-			</div>
-		</main>
-
+		<div className='p-3'>
+			<WelcomeInfo />
+		</div>
 	);
 };
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -4,33 +4,22 @@ import { MainDashboard } from '../dashboard/pages/MainDashboard';
 import { Error404Page } from '../error-404/pages/Error404Page';
 import { PatientsDashboard } from '../dashboard/pages/PatientsDashboard';
 import { TurnsDashboard } from '../dashboard/pages/TurnsDashboard';
-
-import { NavbarLayout } from '../ui/pages/NavbarLayout';
-
 import { DetailPlansPage } from '../plan-details/pages/DetailPlansPage';
 import { DashboardLayout } from '../dashboard/ui/DashboardLayout';
-
 
 export const AppRouter = () => {
 	return (
 		<div>
 			<BrowserRouter>
 				<Routes>
-
-					<Route element={<NavbarLayout />}>
-						<Route index element={<HomePage />} />
-						<Route path='*' element={<Error404Page />} />
-					</Route>
-					<Route path='/dashboard' element={<MainDashboard />}>
-
+					<Route path='/' element={<HomePage />} />
+					<Route path='/dashboard' element={<DashboardLayout />}>
+						<Route index element={<MainDashboard />} />
 						<Route path='patients' element={<PatientsDashboard />} />
 						<Route path='turns' element={<TurnsDashboard />} />
 					</Route>
-
-
 					<Route path='/detail-plans/:name' element={<DetailPlansPage />} />
 					<Route path='*' element={<Error404Page />} />
-
 				</Routes>
 			</BrowserRouter>
 		</div>
