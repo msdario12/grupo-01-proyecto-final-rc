a32423dd904b2d5249c37b07b34382b7e7f49eea
msdario12 <mansilladario94@gmail.com>
2023-07-26 18:34:22 -0300

    se termina de implemntar cambios del merge con el nav

--- src/dashboard/ui/DashboardLayout.jsx
+++ src/dashboard/ui/DashboardLayout.jsx
@@ -1,14 +1,28 @@
 import { Outlet } from 'react-router';
 import { SideMenu } from '../components/SideMenu';
+import { MainNavBar } from '../../ui/components/MainNavBar';
+import { useState } from 'react';
 
 export const DashboardLayout = () => {
+	const [isSideBarOpen, setIsSideBarOpen] = useState(true);
 	return (
-		<main className='container-fluid ps-0 '>
+		<main className={`container-fluid ${isSideBarOpen && 'ps-0'}`}>
 			<div className='row'>
-				<div className='col-3 min-vh-100'>
+				<div
+					className={`min-vh-100 pe-0 ${
+						isSideBarOpen ? 'col-2 col-md-3 col-lg-2' : 'd-none'
+					}`}>
 					<SideMenu />
 				</div>
-				<div className='col-9'>
+				<div
+					className={`px-0 ${
+						isSideBarOpen ? 'col-10 col-md-9 col-lg-10' : 'col-12'
+					}`}>
+					<MainNavBar
+						isInDashboard={true}
+						setIsSideBarOpen={setIsSideBarOpen}
+						isSideBarOpen={isSideBarOpen}
+					/>
 					<Outlet />
 				</div>
 			</div>
