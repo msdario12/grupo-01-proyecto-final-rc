69f1b3d8fde195b59bdeb0b69e18b0915ffb691e
msdario12 <mansilladario94@gmail.com>
2023-07-26 17:22:45 -0300

    diseño responsive de las cards

--- src/dashboard/components/WelcomeInfo.jsx
+++ src/dashboard/components/WelcomeInfo.jsx
@@ -7,27 +7,27 @@ export const WelcomeInfo = () => {
 				Bienvenido de vuelta Otto!
 			</h2>
 			<div className='row gy-3 gx-3'>
-				<div className='col-auto '>
-					<Card
-						style={{ height: 150 }}
-						className='border-0 bg-light rounded-0 shadow-sm '>
-						<Card.Body className='d-flex flex-column'>
-							<p className='text-muted fs-6 mb-1 fw-bold'>
-								Turnos de hoy - 26/07/23
-							</p>
-							<hr className='my-1'></hr>
+				<div className='col-md-4 '>
+					<Card className='h-100 border-0 bg-light rounded-0 shadow-sm '>
+						<Card.Body className='d-flex flex-column justify-content-between'>
+							<div>
+								<p className='text-muted fs-6 mb-1 fw-bold'>
+									Turnos de hoy - 26/07/23
+								</p>
+								<hr className='my-1'></hr>
+							</div>
 							<div className='d-flex gap-2 align-items-center'>
 								<h2 className=''>232</h2>
 								<span className='text-muted'>Turnos programados</span>
 							</div>
 
 							<hr className='my-1'></hr>
-							<div className='d-flex gap-3'>
-								<div className='d-flex gap-1 align-items-baseline'>
+							<div className='row gy-3'>
+								<div className='col-xl-6 d-flex gap-1 align-items-baseline'>
 									<h5>42</h5>
 									<span className='text-muted '>Turnos completados</span>
 								</div>
-								<div className='d-flex gap-1 align-items-baseline'>
+								<div className='col-xl-6 d-flex gap-1 align-items-baseline'>
 									<h5>156</h5>
 									<span className='text-muted'>Turnos pendientes</span>
 								</div>
@@ -35,22 +35,22 @@ export const WelcomeInfo = () => {
 						</Card.Body>
 					</Card>
 				</div>
-				<div className='col-auto '>
-					<Card
-						style={{ height: 150 }}
-						className='border-0 bg-light rounded-0 shadow-sm'>
-						<Card.Body className='d-flex flex-column'>
-							<p className='text-muted fs-6 mb-1 fw-bold'>Próximos turnos</p>
-							<hr className='my-1'></hr>
-							<div className='d-flex gap-3 my-auto'>
-								<div className='d-flex gap-2 align-items-center'>
+				<div className='col-md-4 '>
+					<Card className='h-100 border-0 bg-light rounded-0 shadow-sm'>
+						<Card.Body className='d-flex flex-column justify-content-between'>
+							<div>
+								<p className='text-muted fs-6 mb-1 fw-bold'>Próximos turnos</p>
+								<hr className='my-1'></hr>
+							</div>
+							<div className='row my-auto gy-3'>
+								<div className='col-6 col-md-12 col-xl-6 d-flex gap-2 align-items-center'>
 									<h2 className=''>14:30</h2>
 									<div className='d-flex flex-column justify-content-center align-items-end'>
 										<span className='text-muted'>Juan Perez</span>
 										<span className='text-muted'>"Luna" (Gato)</span>
 									</div>
 								</div>
-								<div className='d-flex gap-2 align-items-center'>
+								<div className='col-6 col-md-12 col-xl-6 d-flex gap-2 align-items-center'>
 									<h2 className=''>15:30</h2>
 									<div className='d-flex flex-column justify-content-center align-items-end'>
 										<span className='text-muted'>Pedro Sanchez</span>
@@ -61,15 +61,15 @@ export const WelcomeInfo = () => {
 						</Card.Body>
 					</Card>
 				</div>
-				<div className='col-auto '>
-					<Card
-						style={{ height: 150 }}
-						className='border-0 bg-light rounded-0 shadow-sm'>
+				<div className='col-md-4 '>
+					<Card className='h-100 border-0 bg-light rounded-0 shadow-sm'>
 						<Card.Body className='d-flex flex-column justify-content-between'>
-							<p className=' text-muted fs-6 mb-1 fw-bold'>
-								Estadísticas de Atención y Resumen de Mascotas
-							</p>
-							<hr className='my-1'></hr>
+							<div>
+								<p className=' text-muted fs-6 mb-1 fw-bold'>
+									Estadísticas de Atención y Resumen de Mascotas
+								</p>
+								<hr className='my-1'></hr>
+							</div>
 							<div className='d-flex gap-2 align-items-center'>
 								<span className='text-muted'>
 									Mascotas atendidas esta semana:{' '}
@@ -87,27 +87,6 @@ export const WelcomeInfo = () => {
 								</span>
 								<h5 className=''>425</h5>
 							</div>
-						</Card.Body>
-					</Card>
-				</div>
-				<div className='col-md-6 '>
-					<Card className='text-center'>
-						<Card.Body>
-							<Card.Title>Nuevos Pacientes</Card.Title>
-						</Card.Body>
-					</Card>
-				</div>
-				<div className='col-md-6 '>
-					<Card className='text-center'>
-						<Card.Body>
-							<Card.Title>Nuevos Pacientes</Card.Title>
-						</Card.Body>
-					</Card>
-				</div>
-				<div className='col-md-6 '>
-					<Card className='text-center'>
-						<Card.Body>
-							<Card.Title>Nuevos Pacientes</Card.Title>
 						</Card.Body>
 					</Card>
 				</div>
