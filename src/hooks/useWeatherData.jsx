466df1046c96ccbd8edac0dbe3acb05e47326b1b
msdario12 <mansilladario94@gmail.com>
2023-07-27 10:26:56 -0300

    se agrega la api para obtener la ubicacion del cliente y en base a esta hacer la consulta del estado del tiempo

--- src/hooks/useWeatherData.jsx
+++ src/hooks/useWeatherData.jsx
@@ -5,38 +5,43 @@ const timeToWaitInSeconds = 300;
 
 export const useWeatherData = () => {
 	const [weatherData, setWeatherData] = useState({});
-	useEffect(() => {
-		const now = new Date().getTime();
-		const localFetch = localStorage.getItem('lastFetchWeather');
-		if (!localFetch) {
-			console.log('no hay data');
-			localStorage.setItem('lastFetchWeather', now);
-		}
-		if (now - localFetch < timeToWaitInSeconds * 1000) {
-			console.log('no fetch');
-			const cachedData = localStorage.getItem('weatherData');
-			if (cachedData) {
-				setWeatherData(JSON.parse(cachedData));
-			}
-			return;
-		}
-		localStorage.setItem('lastFetchWeather', now);
-		console.log('nuevo fetch');
 
-		backendAPI
-			.get('/api/weather', {
-				params: {
-					location: 'San miguel de Tucuman',
-				},
-			})
-			.then((res) => {
-				setWeatherData(res.data.data);
-				localStorage.setItem('weatherData', JSON.stringify(res.data.data));
-			})
-			.catch((e) => {
-				console.log(e);
+	useEffect(() => {
+		// obtener localizacion
+		navigator.geolocation.getCurrentPosition(function (position) {
+			const now = new Date().getTime();
+			const localFetch = localStorage.getItem('lastFetchWeather');
+			if (!localFetch) {
+				console.log('no hay data');
 				localStorage.setItem('lastFetchWeather', now);
-			});
+			}
+			if (now - localFetch < timeToWaitInSeconds * 1000) {
+				console.log('no fetch');
+				const cachedData = localStorage.getItem('weatherData');
+				if (cachedData) {
+					setWeatherData(JSON.parse(cachedData));
+				}
+				return;
+			}
+			localStorage.setItem('lastFetchWeather', now);
+			console.log('nuevo fetch');
+
+			backendAPI
+				.get('/api/weather', {
+					params: {
+						location: `${position.coords.latitude}, ${position.coords.longitude}`,
+					},
+				})
+				.then((res) => {
+					setWeatherData(res.data.data);
+					localStorage.setItem('weatherData', JSON.stringify(res.data.data));
+				})
+				.catch((e) => {
+					console.log(e);
+					localStorage.setItem('lastFetchWeather', now);
+				});
+		});
 	}, []);
+
 	return [weatherData];
 };
