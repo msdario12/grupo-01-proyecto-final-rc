38b72165717930084b89be901de960f2308c33c5
msdario12 <mansilladario94@gmail.com>
2023-07-26 23:04:58 -0300

    se hace la peticion y se muestran datos del tiempo

--- src/ui/components/WeatherData.jsx
+++ src/ui/components/WeatherData.jsx
@@ -1,16 +1,33 @@
+import { useEffect, useState } from 'react';
+import { backendAPI } from '../../api/backendAPI';
+
 export const WeatherData = () => {
+	const [weatherData, setWeatherData] = useState({});
+	useEffect(() => {
+		backendAPI
+			.get('/api/weather', {
+				params: {
+					location: 'San miguel de Tucuman',
+				},
+			})
+			.then((res) => setWeatherData(res.data.data));
+	}, []);
+	console.log(weatherData);
+	if (!weatherData.current) {
+		return 'Cargando datos...';
+	}
 	return (
 		<div className='d-flex'>
 			<div className=''>
 				<img
 					className='img-fluid'
-					src='http://cdn.weatherapi.com/weather/64x64/night/113.png'
+					src={weatherData.current.condition.icon}
 					alt='Weather icon'
 				/>
 			</div>
 			<div className='d-flex flex-column my-0 justify-content-center'>
-				<p className='fw-bold fs-4 my-0'>23°C</p>
-				<span className='d-none d-md-block'>San Miguel de Tucumán</span>
+				<p className='fw-bold fs-4 my-0'>{weatherData.current.temp_c}°C</p>
+				<span className='d-none d-md-block'>{weatherData.location.name}</span>
 			</div>
 		</div>
 	);
