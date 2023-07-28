965793f110c2d1b6ddb93bb93c9b2a75249b6dce
msdario12 <mansilladario94@gmail.com>
2023-07-28 19:15:32 -0300

    se mueve el componente ThCustom a un nuevo archivo

--- src/dashboard/components/MainTableTurns.jsx
+++ src/dashboard/components/MainTableTurns.jsx
@@ -1,9 +1,8 @@
-import { faSort } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import format from 'date-fns/format';
 import { es } from 'date-fns/locale';
 import { useEffect, useState } from 'react';
 import { Badge, Table } from 'react-bootstrap';
+import { CustomTh } from './CustomTh';
 
 const turnsListObject = [
 	{
@@ -63,62 +62,6 @@ const turnsListObject = [
 	},
 ];
 
-export const CustomTh = ({ title, name, setTurnsList }) => {
-	const [sortMode, setSortMode] = useState('ascend');
-
-	const switchSortMode = () => {
-		if (sortMode === 'original') {
-			setSortMode('descend');
-			return;
-		}
-		if (sortMode === 'descend') {
-			setSortMode('ascend');
-			return;
-		}
-		if (sortMode === 'ascend') {
-			setSortMode('original');
-			return;
-		}
-	};
-
-	const comparingFunction = (a, b, name) => {
-		console.log(typeof a[name], sortMode);
-		if (sortMode === 'original') {
-			return a['id'] - b['id'];
-		}
-		const nameA = typeof a[name] === 'string' ? a[name].toUpperCase() : a[name]; // ignore upper and lowercase
-		const nameB = typeof b[name] === 'string' ? b[name].toUpperCase() : b[name]; // ignore upper and lowercase
-
-		if (nameA < nameB) {
-			return sortMode === 'descend' ? 1 : -1;
-		}
-		if (nameA > nameB) {
-			return sortMode === 'descend' ? -1 : 1;
-		}
-		// names must be equal
-		return 0;
-	};
-	const sortByName = (name) => {
-		console.log(sortMode);
-		switchSortMode();
-		setTurnsList((prev) =>
-			[...prev].sort((a, b) => comparingFunction(a, b, name))
-		);
-	};
-	return (
-		<th className='text-muted small'>
-			<div className='d-flex align-items-center justify-content-between'>
-				<span className='me-2'>{title}</span>
-				<FontAwesomeIcon
-					style={{ cursor: 'pointer' }}
-					icon={faSort}
-					onClick={() => sortByName(name)}
-				/>
-			</div>
-		</th>
-	);
-};
-
 export const MainTableTurns = () => {
 	const [turnsList, setTurnsList] = useState([]);
 	useEffect(() => {
@@ -143,7 +86,11 @@ export const MainTableTurns = () => {
 						title={'Fecha'}
 						name={'dateObj'}
 					/>
-					<CustomTh setTurnsList={setTurnsList} title={'Hora'} name={'time'} />
+					<CustomTh
+						setTurnsList={setTurnsList}
+						title={'Hora'}
+						name={'dateObj'}
+					/>
 					<CustomTh
 						setTurnsList={setTurnsList}
 						title={'Cliente'}
