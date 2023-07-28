b22883b330adcdc4b2c8d1f04f784874162d1d5f
msdario12 <mansilladario94@gmail.com>
2023-07-28 20:31:29 -0300

    ajuste de espacios verticales

--- src/dashboard/pages/MainDashboard.jsx
+++ src/dashboard/pages/MainDashboard.jsx
@@ -3,13 +3,15 @@ import { WelcomeInfo } from '../components/WelcomeInfo';
 
 export const MainDashboard = () => {
 	return (
-		<div className='py-3 container d-flex flex-column gap-5'>
-			<div className='my-2 my-md-5'>
+		<div className='py-1 py-lg-3 container d-flex flex-column gap-3 gap-lg-5'>
+			<div className='mt-1 mt-md-2 mt-lg-4'>
 				<h2 style={{ letterSpacing: 0.1 }} className='display-4 fw-bold'>
 					Bienvenido de vuelta{' '}
 					<span className='px-2 text-bg-secondary text-light'>Otto!</span>
 				</h2>
-				<h4 className='display-6 fs-4'>Esto son los principales datos de hoy:</h4>
+				<h4 className='display-6 fs-4'>
+					Esto son los principales datos de hoy:
+				</h4>
 			</div>
 			<WelcomeInfo />
 			<MainTableTurns />
