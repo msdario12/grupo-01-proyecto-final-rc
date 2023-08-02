52d7595b3e2b9b5d909e83b96c261828e4d0aeb9
msdario12 <mansilladario94@gmail.com>
2023-08-02 18:23:38 -0300

    se agrega el authProvider en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { AuthProvider } from './context/AuthProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<AuthProvider>
+			<App />
+		</AuthProvider>
 	</React.StrictMode>
 );
