a3942e602a9468297beebfc607f1e229265a225b
Homet Matias <mhometherrera@gmail.com>
2023-08-02 15:59:47 -0300

    Se agrego estilos al footer

--- src/home/components/Footer.jsx
+++ src/home/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import "./Footer.css"
 
 export const Footer = () => {
   return (
     <>
     
-    <footer className="page-footer font-small blue pt-4">
-    <div className="container-fluid text-center text-md-left">
+    <footer className="page-footer font-small blue pt-4 footer-container">
+    <div className="container text-center text-left">
         <div className="row">
             <div className="col-md-6 mt-md-0 mt-3">
                 <h5 className="text-uppercase">Footer Content</h5>
--- src/home/pages/HomePage.jsx
+++ src/home/pages/HomePage.jsx
@@ -1,16 +1,18 @@
-import { ComparisonPlans } from '../components/ComparisonPlans';
-import { Footer } from '../components/Footer';
-import { HeroSection } from '../components/HeroSection';
-import { TestimonialSection } from '../components/TestimonialSection';
+import { ComparisonPlans } from "../components/ComparisonPlans";
+import { Footer } from "../components/Footer";
+import { HeroSection } from "../components/HeroSection";
+import { TestimonialSection } from "../components/TestimonialSection";
 
 export const HomePage = () => {
-	return (
-		<main className='container-lg'>
-			<h2>Esto es el HomePage</h2>
-			<HeroSection />
-			<ComparisonPlans />
-			<TestimonialSection />
-			<Footer />
-		</main>
-	);
+  return (
+    <>
+      <main className="container-lg">
+        <h2>Esto es el HomePage</h2>
+        <HeroSection />
+        <ComparisonPlans />
+        <TestimonialSection />
+      </main>
+      <Footer />
+    </>
+  );
 };
