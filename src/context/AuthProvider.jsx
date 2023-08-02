8531e594d3147df068092d00fb20010388751190
msdario12 <mansilladario94@gmail.com>
2023-08-02 18:22:59 -0300

    se crea el auth provider y context

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { createContext } from 'react';
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+	const [auth, setAuth] = useState({});
+	return (
+		<AuthContext.Provider value={{ auth, setAuth }}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
