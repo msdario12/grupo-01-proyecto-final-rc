02ccdfc058a9eeba2f5a435d28d48f0adacb145f
msdario12 <mansilladario94@gmail.com>
2023-08-02 19:27:16 -0300

    se crea el provider y context para el modal, se lo monta en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthProvider.jsx';
+import { ModalProvider } from './context/ModalContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<AuthProvider>
-			<App />
+			<ModalProvider>
+				<App />
+			</ModalProvider>
 		</AuthProvider>
 	</React.StrictMode>
 );
