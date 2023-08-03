f34a29e1f78a144055b67699009ea275f948bf3e
Homet Matias <mhometherrera@gmail.com>
2023-08-03 15:38:17 -0300

    Se creo un customhook

--- src/Turns/components/TurnForm.jsx
+++ src/Turns/components/TurnForm.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react";
+import Form from "react-bootstrap/Form";
 
 export const TurnForm = () => {
   return (
     <>
-    
-        <h2>Reserva tu turno</h2>
+      <h2>Reserva tu turno</h2>
 
-        
 
     </>
-  )
-}
+  );
+};
