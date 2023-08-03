fb9af5fa741097a124e8dba7a72a06c7e33aebb3
Homet Matias <mhometherrera@gmail.com>
2023-08-03 14:45:38 -0300

    Se creo la pagina de Turnos

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,69 +1,74 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { HomePage } from '../home/pages/HomePage';
-import { MainDashboard } from '../dashboard/pages/MainDashboard';
-import { Error404Page } from '../error-404/pages/Error404Page';
-import { PatientsDashboard } from '../dashboard/pages/PatientsDashboard';
-import { TurnsDashboard } from '../dashboard/pages/TurnsDashboard';
-import { AboutUs } from '../home/pages/About-us';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "../home/pages/HomePage";
+import { MainDashboard } from "../dashboard/pages/MainDashboard";
+import { Error404Page } from "../error-404/pages/Error404Page";
+import { PatientsDashboard } from "../dashboard/pages/PatientsDashboard";
+import { TurnsDashboard } from "../dashboard/pages/TurnsDashboard";
+import { AboutUs } from "../home/pages/About-us";
 
-import { ContactPage } from '../contact/pages/ContactPage';
+import { ContactPage } from "../contact/pages/ContactPage";
 
-import { DetailPlansPage } from '../plan-details/pages/DetailPlansPage';
-import { DashboardLayout } from '../dashboard/ui/DashboardLayout';
-import { NavbarLayout } from '../ui/pages/NavbarLayout';
-import { NewPatientForm } from '../dashboard/components/NewPatientForm';
-import { RequireAuth } from '../auth/components/RequireAuth';
-import { GenericModal } from '../ui/components/GenericModal';
+import { DetailPlansPage } from "../plan-details/pages/DetailPlansPage";
+import { DashboardLayout } from "../dashboard/ui/DashboardLayout";
+import { NavbarLayout } from "../ui/pages/NavbarLayout";
+import { NewPatientForm } from "../dashboard/components/NewPatientForm";
+import { RequireAuth } from "../auth/components/RequireAuth";
+import { GenericModal } from "../ui/components/GenericModal";
+import { TurnPage } from "../Turns/pages/TurnPage";
 
 export const AppRouter = () => {
-	return (
-		<div>
-			<BrowserRouter>
-				<GenericModal />
-				<Routes>
-					{/* Estas rutas son públicas */}
-					<Route element={<NavbarLayout />}>
-						<Route index element={<HomePage title={'Home'} />} />
-						<Route
-							path='contact'
-							element={<ContactPage title={'Contáctenos'} />}
-						/>
-						<Route
-							path='/detail-plans/:name'
-							element={<DetailPlansPage title={'Nuestros planes'} />}
-						/>
-						<Route
-							path='*'
-							element={<Error404Page title={'Pagina no encontrada'} />}
-						/>
-						<Route
-							path='about-us'
-							element={<AboutUs title={'Sobre nosotros'} />}
-						/>
-					</Route>
-					{/* Estas rutas son solo para administrador - protegidas */}
-					<Route element={<RequireAuth />}>
-						<Route path='/dashboard' element={<DashboardLayout />}>
-							<Route
-								index
-								element={<MainDashboard title={'Panel administrador'} />}
-							/>
-							<Route
-								path='patients'
-								element={<PatientsDashboard title={'Pacientes'} />}
-							/>
-							<Route
-								path='turns'
-								element={<TurnsDashboard title={'Turnos'} />}
-							/>
-							<Route
-								path='add-patient'
-								element={<NewPatientForm title={'Añadir paciente'} />}
-							/>
-						</Route>
-					</Route>
-				</Routes>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div>
+      <BrowserRouter>
+        <GenericModal />
+        <Routes>
+          {/* Estas rutas son públicas */}
+          <Route element={<NavbarLayout />}>
+            <Route index element={<HomePage title={"Home"} />} />
+            <Route
+              path="contact"
+              element={<ContactPage title={"Contáctenos"} />}
+            />
+            <Route
+              path="/detail-plans/:name"
+              element={<DetailPlansPage title={"Nuestros planes"} />}
+            />
+            <Route
+              path="*"
+              element={<Error404Page title={"Pagina no encontrada"} />}
+            />
+            <Route
+              path="about-us"
+              element={<AboutUs title={"Sobre nosotros"} />}
+            />
+            <Route
+              path="turns"
+              element={<TurnPage title={"Turnos"} />}
+            />
+          </Route>
+          {/* Estas rutas son solo para administrador - protegidas */}
+          <Route element={<RequireAuth />}>
+            <Route path="/dashboard" element={<DashboardLayout />}>
+              <Route
+                index
+                element={<MainDashboard title={"Panel administrador"} />}
+              />
+              <Route
+                path="patients"
+                element={<PatientsDashboard title={"Pacientes"} />}
+              />
+              <Route
+                path="turns"
+                element={<TurnsDashboard title={"Turnos"} />}
+              />
+              <Route
+                path="add-patient"
+                element={<NewPatientForm title={"Añadir paciente"} />}
+              />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
