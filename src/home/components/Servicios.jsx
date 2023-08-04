f0bdbff9bae98541589ac34e85a4d1cb20137663
melany <mel18rodriguez@gmail.com>
2023-08-04 20:07:21 -0300

    CAMBIE ALGUNAS IMAGENES, EL ANCHO DEL CAROUSEL , Y AGREGUE DESCRIPCIONES

--- src/home/components/Servicios.jsx
+++ src/home/components/Servicios.jsx
@@ -7,71 +7,71 @@ export const Servicios = () => {
 	return (
 		<>
 
-<h2 className='bb'>Nuestros Servicios </h2>
+<h2 >Nuestros Servicios </h2>
 
     <Carousel className= "Carousel"  data-bs-theme="dark">
       <Carousel.Item  className='c-item' >
-        <img
-          className="d-block w-100   .c-img "
+        <img 
+          className="d-block w-100   c-img "
           src="https://i.ibb.co/v1Xk18b/pexels-photo-6235233.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h5>EXAMENES MEDICOS COMPLETOS </h5>
+          <p>Consulta medica general, problemas de piel, perros y/o gatos con sintomas de vomitos y diarrea. Dermatogologia</p>
         </Carousel.Caption>
       </Carousel.Item>
 
 
       <Carousel.Item className='c-item' >
         <img
-          className="d-block w-100   .c-img"
+          className="d-block w-100   c-img"
           src="https://i.ibb.co/vHcn187/pexels-photo-6235663.jpg"
           alt="Second slide"
         />
         <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h5>MONITOREO CARDIACO Y RESPIRATORIO</h5>
+          <p>Servicio de electrocardiograma y diagnóstico cardiológico.</p>
         </Carousel.Caption>
       </Carousel.Item>
 
       
       <Carousel.Item className='c-item'  >
         <img
-          className="d-block w-100   .c-img "
+          className="d-block w-100   c-img "
           src="https://i.ibb.co/JWLJnqL/pexels-photo-6816869.jpg"
           alt="Third slide"
         />
         <Carousel.Caption>
-          <h5>Third slide label</h5>
+          <h5>VACUNAS Y REFUERZOS</h5>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          Plan de vacunación para cachorros y adultos. Quintuple, sextuple, antirrabica, triple felina, leucemia felina.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item className='c-item'>
         <img
-          className="d-block w-100    .c-img"
-          src="https://i.ibb.co/N6skH6W/pexels-photo-6816861.jpg"
+          className="d-block w-100    c-img"
+          src="https://i.ibb.co/SdSqXwg/ccn-dental-care-oral-checks-w-640-fm-jpg-auto-compress.jpg"
           alt="Third slide"
         />
         <Carousel.Caption>
-          <h5>Third slide label</h5>
+          <h5>CUIDADO DENTAL</h5>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          El cuidado de sus dientes debe empezar cuando aun es cachorro. Esto te permitirá educarlo correctamente para permitir el cepillado y las inspecciones que deba realizar su veterinario.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item   className='c-item'>
         <img
-          className="d-block w-100   .c-img "
-          src="https://i.ibb.co/kctqMRW/pexels-photo-7474852.jpg"
+          className="d-block w-100   c-img "
+          src="https://i.ibb.co/hKSjNSK/ed62h-how-your-dog-s-nutrition-needs-change-with-age-hero-dog-w-1280-fm-jpg-auto-compress.jpg"
           alt="Third slide"
         />
         <Carousel.Caption>
-          <h5>Third slide label</h5>
+          <h5>GERIATRIA VETERINARIA</h5>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
