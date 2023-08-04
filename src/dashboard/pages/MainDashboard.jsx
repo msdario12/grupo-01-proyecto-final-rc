9d198f43374f0d3d1eef66a5c9a5e75e06901634
msdario12 <mansilladario94@gmail.com>
2023-08-04 18:20:11 -0300

    se quita boton de prueba en el main dashboard

--- src/dashboard/pages/MainDashboard.jsx
+++ src/dashboard/pages/MainDashboard.jsx
@@ -6,13 +6,10 @@ import { backendAPI } from '../../api/backendAPI';
 
 export const MainDashboard = ({ title }) => {
 	useDocumentTitle(title);
-	const handleTest = () => {
-		backendAPI.get('/api/turns').then((res) => console.log('Enviado'));
-	};
+
 	return (
 		<div>
 			<div className='mt-1 mt-md-2 mt-lg-4'>
-				<Button onClick={handleTest}>Test mensaje</Button>
 				<h2 style={{ letterSpacing: 0.1 }} className='display-4 fw-bold'>
 					Bienvenido de vuelta{' '}
 					<span className='px-2 text-bg-secondary text-light'>Otto!</span>
