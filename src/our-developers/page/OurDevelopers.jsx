6b361df7f9b5b180bdb2e9943c7785953fd309fb
Homet Matias <mhometherrera@gmail.com>
2023-08-04 14:43:25 -0300

    Se relleno de informacion la segunda card

--- src/our-developers/page/OurDevelopers.jsx
+++ src/our-developers/page/OurDevelopers.jsx
@@ -19,15 +19,15 @@ export const OurDevelopers = () => {
       </Card>
 
       <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="https://i.ibb.co/BVR0JWH/as.jpg" />
+        <Card.Img variant="top" src="https://i.ibb.co/7Gcrc0w/perfil-dario.jpg" />
         <Card.Body>
-          <Card.Title>Homet Matias</Card.Title>
+          <Card.Title>Dario Mansilla</Card.Title>
         </Card.Body>
         <ListGroup className="list-group-flush">
           <ListGroup.Item>Bueno en Front</ListGroup.Item>
-          <ListGroup.Item>Flojo en Backend (Mejorando)</ListGroup.Item>
+          <ListGroup.Item>Bueno en Backend</ListGroup.Item>
           <ListGroup.Item>Responsable</ListGroup.Item>
-          <ListGroup.Item>Impuntual (Mejorando)</ListGroup.Item>
+          <ListGroup.Item>Puntual</ListGroup.Item>
         </ListGroup>
       </Card>
     </>
