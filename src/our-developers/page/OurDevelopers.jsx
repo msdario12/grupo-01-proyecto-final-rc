d2c83005fdf438773721864be7c535352853c684
Homet Matias <mhometherrera@gmail.com>
2023-08-04 14:44:17 -0300

    Se agrego la tercera card

--- src/our-developers/page/OurDevelopers.jsx
+++ src/our-developers/page/OurDevelopers.jsx
@@ -30,6 +30,19 @@ export const OurDevelopers = () => {
           <ListGroup.Item>Puntual</ListGroup.Item>
         </ListGroup>
       </Card>
+
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="https://i.ibb.co/7Gcrc0w/perfil-dario.jpg" />
+        <Card.Body>
+          <Card.Title>Dario Mansilla</Card.Title>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroup.Item>Bueno en Front</ListGroup.Item>
+          <ListGroup.Item>Bueno en Backend</ListGroup.Item>
+          <ListGroup.Item>Responsable</ListGroup.Item>
+          <ListGroup.Item>Puntual</ListGroup.Item>
+        </ListGroup>
+      </Card>
     </>
   );
 };
