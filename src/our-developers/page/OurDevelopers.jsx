c943c7042e17ec9ea934a1b9562e9ed8d27cd2b2
Homet Matias <mhometherrera@gmail.com>
2023-08-04 14:45:19 -0300

    Se termino de rellenar de informacion la terce card

--- src/our-developers/page/OurDevelopers.jsx
+++ src/our-developers/page/OurDevelopers.jsx
@@ -34,11 +34,11 @@ export const OurDevelopers = () => {
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src="https://i.ibb.co/7Gcrc0w/perfil-dario.jpg" />
         <Card.Body>
-          <Card.Title>Dario Mansilla</Card.Title>
+          <Card.Title>Melany Rodriguez</Card.Title>
         </Card.Body>
         <ListGroup className="list-group-flush">
-          <ListGroup.Item>Bueno en Front</ListGroup.Item>
-          <ListGroup.Item>Bueno en Backend</ListGroup.Item>
+          <ListGroup.Item>Buena en Front</ListGroup.Item>
+          <ListGroup.Item>Floja en Backend (Mejorando)</ListGroup.Item>
           <ListGroup.Item>Responsable</ListGroup.Item>
           <ListGroup.Item>Puntual</ListGroup.Item>
         </ListGroup>
