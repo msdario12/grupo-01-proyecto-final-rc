d310614ce324a5f88dfac75603a2f5292dd96071
msdario12 <mansilladario94@gmail.com>
2023-08-10 10:13:36 -0300

    se cambia la url del backend para que sea una variable de entorno

--- src/api/backendAPI.js
+++ src/api/backendAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'https://backend-grupo-01-proyecto-final-rc-zorb-dev.fl0.io';
+const URL = import.meta.env.VITE_BACKEND_URL;
 
 export const backendAPI = axios.create({
 	baseURL: URL,
