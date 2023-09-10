94173d361ec0990eb6db09e14323609228cabdc7
msdario12 <mansilladario94@gmail.com>
2023-09-09 23:26:15 -0300

    se agrega funcion de crear turnos desde pagina sin registro, formateo de codigo

--- src/dashboard/turns/components/TurnsInputForm.jsx
+++ src/dashboard/turns/components/TurnsInputForm.jsx
@@ -1,82 +1,84 @@
-import { Form, FormControl } from 'react-bootstrap';
+import { Form, FormControl } from "react-bootstrap";
 import {
-	filterPassedTime,
-	isWeekday,
-	vetList,
-} from '../../schema-validations/turnSchema';
-import ReactDatePicker from 'react-datepicker';
-import { es } from 'date-fns/locale';
-import { InputWithFeedback } from '../../../plan-details/elements/InputWithFeedback';
+  filterPassedTime,
+  isWeekday,
+  vetList,
+} from "../../schema-validations/turnSchema";
+import ReactDatePicker from "react-datepicker";
+import { es } from "date-fns/locale";
+import { InputWithFeedback } from "../../../plan-details/elements/InputWithFeedback";
 
 export const TurnsInputForm = ({ formik }) => {
-	return (
-		<div>
-			<Form.Group className='mb-3' controlId='vet'>
-				<Form.Label>Veterinario para el turno *</Form.Label>
-				<Form.Select
-					className='text-capitalize'
-					name='vet'
-					{...formik.getFieldProps('vet')}
-					isValid={!formik.errors.vet && formik.touched.vet}
-					isInvalid={formik.errors.vet && formik.touched.vet}>
-					<option disabled value={'placeholder'}>
-						Selecciona un veterinario
-					</option>
-					{vetList.map((vet) => (
-						<option key={vet} value={vet}>
-							{vet}
-						</option>
-					))}
-				</Form.Select>
-				<Form.Control.Feedback type='invalid'>
-					{formik.errors.vet}
-				</Form.Control.Feedback>
-			</Form.Group>
-			<Form.Group
-				className='mb-3 d-flex gap-3 align-items-center'
-				controlId='turnDate'>
-				<Form.Label>Selecciona la fecha y hora para el turno *</Form.Label>
-				<FormControl
-					as={ReactDatePicker}
-					name='turnDate'
-					isClearable
-					showIcon
-					autoComplete='off'
-					minDate={new Date()}
-					selected={formik.values.turnDate}
-					onChange={(value) => formik.setFieldValue('turnDate', value)}
-					onSelect={(value) => formik.setFieldValue('turnDate', value)}
-					onBlur={formik.handleBlur}
-					value={formik.values.turnDate}
-					filterDate={isWeekday}
-					placeholderText='Fecha y hora'
-					showTimeSelect
-					filterTime={filterPassedTime}
-					dateFormat='PPp'
-					locale={es}
-					isValid={!formik.errors.turnDate && formik.touched.turnDate}
-					isInvalid={formik.errors.turnDate}
-				/>
-				<Form.Control.Feedback type='invalid'>
-					{formik.errors.turnDate}
-				</Form.Control.Feedback>
-				<span className='text-danger'>{formik.errors.turnDate}</span>
-			</Form.Group>
-			<Form.Group className='mb-3' controlId='details'>
-				<Form.Label>Detalles del turno *</Form.Label>
-				<InputWithFeedback
-					hasTextCapitalization={false}
-					type='text'
-					placeholder='Notas sobre el estado del paciente, síntomas, etc...'
-					formik={formik}
-					name={'details'}
-					props={{
-						maxLength: 255,
-						style: { height: 80 },
-						as: 'textarea',
-					}}
-				/>
-			</Form.Group>
-		</div>
-	);
+  return (
+    <div>
+      <Form.Group className="mb-3" controlId="vet">
+        <Form.Label>Veterinario para el turno *</Form.Label>
+        <Form.Select
+          className="text-capitalize"
+          name="vet"
+          {...formik.getFieldProps("vet")}
+          isValid={!formik.errors.vet && formik.touched.vet}
+          isInvalid={formik.errors.vet && formik.touched.vet}
+        >
+          <option disabled value={"placeholder"}>
+            Selecciona un veterinario
+          </option>
+          {vetList.map((vet) => (
+            <option key={vet} value={vet}>
+              {vet}
+            </option>
+          ))}
+        </Form.Select>
+        <Form.Control.Feedback type="invalid">
+          {formik.errors.vet}
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Form.Group
+        className="mb-3 d-flex gap-3 align-items-center"
+        controlId="turnDate"
+      >
+        <Form.Label>Selecciona la fecha y hora para el turno *</Form.Label>
+        <FormControl
+          as={ReactDatePicker}
+          name="turnDate"
+          isClearable
+          showIcon
+          autoComplete="off"
+          minDate={new Date()}
+          selected={formik.values.turnDate}
+          onChange={(value) => formik.setFieldValue("turnDate", value)}
+          onSelect={(value) => formik.setFieldValue("turnDate", value)}
+          onBlur={formik.handleBlur}
+          value={formik.values.turnDate}
+          filterDate={isWeekday}
+          placeholderText="Fecha y hora"
+          showTimeSelect
+          filterTime={filterPassedTime}
+          dateFormat="PPp"
+          locale={es}
+          isValid={!formik.errors.turnDate && formik.touched.turnDate}
+          isInvalid={formik.errors.turnDate}
+        />
+        <Form.Control.Feedback type="invalid">
+          {formik.errors.turnDate}
+        </Form.Control.Feedback>
+        <span className="text-danger">{formik.errors.turnDate}</span>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="details">
+        <Form.Label>Detalles del turno *</Form.Label>
+        <InputWithFeedback
+          hasTextCapitalization={false}
+          type="text"
+          placeholder="Notas sobre el estado del paciente, síntomas, etc..."
+          formik={formik}
+          name={"details"}
+          props={{
+            maxLength: 255,
+            style: { height: 80 },
+            as: "textarea",
+          }}
+        />
+      </Form.Group>
+    </div>
+  );
 };
--- src/turn-reservation/pages/TurnReservationPage.jsx
+++ src/turn-reservation/pages/TurnReservationPage.jsx
@@ -1,143 +1,192 @@
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import emailjs from '@emailjs/browser';
-import { UsersInputsForm } from '../../dashboard/patients/components/UsersInputsForm';
-import { useDocumentTitle } from '../../hooks/useDocumentTitle';
-import { Button, Col, Form, Row, Spinner } from 'react-bootstrap';
-import { userSchema } from '../../dashboard/schema-validations/userSchema';
-import { petSchema } from '../../dashboard/schema-validations/petSchema';
-import { useRef, useState } from 'react';
-import { PetInputsForm } from '../../dashboard/patients/components/PetInputsForm';
-import { TurnsInputForm } from '../../dashboard/turns/components/TurnsInputForm';
-import { HeaderTitleDashboard } from '../../dashboard/elements/HeaderTitleDashboard';
-import { turnEditSchema } from '../../dashboard/schema-validations/turnSchema';
-import { CustomAlertResponse } from '../../dashboard/ui/components/CustomAlertResponse';
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import emailjs from "@emailjs/browser";
+import { UsersInputsForm } from "../../dashboard/patients/components/UsersInputsForm";
+import { useDocumentTitle } from "../../hooks/useDocumentTitle";
+import { Button, Col, Form, Row, Spinner } from "react-bootstrap";
+import { userSchema } from "../../dashboard/schema-validations/userSchema";
+import { petSchema } from "../../dashboard/schema-validations/petSchema";
+import { useRef, useState } from "react";
+import { PetInputsForm } from "../../dashboard/patients/components/PetInputsForm";
+import { TurnsInputForm } from "../../dashboard/turns/components/TurnsInputForm";
+import { HeaderTitleDashboard } from "../../dashboard/elements/HeaderTitleDashboard";
+import { turnEditSchema } from "../../dashboard/schema-validations/turnSchema";
+import { CustomAlertResponse } from "../../dashboard/ui/components/CustomAlertResponse";
+import { useAxiosPrivate } from "../../hooks/useAxiosPrivate";
 
 const turnReservationSchema = Yup.object({
-	...userSchema,
-	...petSchema,
-	...turnEditSchema,
+    ...userSchema,
+    ...petSchema,
+    ...turnEditSchema,
 });
 
 export const TurnReservationPage = ({ title }) => {
-	useDocumentTitle(title);
-	const [isLoading, setIsLoading] = useState(false);
-	const [response, setResponse] = useState({
-		success: true,
-		message: '',
-	});
-	const [showAlert, setShowAlert] = useState(false);
-	const form = useRef();
+    useDocumentTitle(title);
+    const [isLoading, setIsLoading] = useState(false);
+    const [response, setResponse] = useState({
+        success: true,
+        message: "",
+    });
+    const [showAlert, setShowAlert] = useState(false);
+    const form = useRef();
 
-	const initialValues = {
-		email: '',
-		firstName: '',
-		lastName: '',
-		phone: '',
-		name: '',
-		specie: 'placeholder',
-		race: '',
-		vet: '',
-		date: '',
-		details: '',
-	};
-	const formik = useFormik({
-		initialValues: initialValues,
-		validationSchema: turnReservationSchema,
-		onSubmit: () => {
-			setIsLoading(true);
-			emailjs
-				.sendForm(
-					'service_pzdzjgj',
-					'template_s8mdv36',
-					form.current,
-					'IA7y8FZdE1Zr4Ky_M'
-				)
-				.then(
-					() => {
-						setShowAlert(true);
-						setIsLoading(false);
+    const { privateBackendAPI } = useAxiosPrivate();
+    const initialValues = {
+        email: "",
+        firstName: "",
+        lastName: "",
+        phone: "",
+        name: "",
+        specie: "placeholder",
+        race: "",
+        vet: "",
+        turnDate: "",
+        details: "",
+    };
+    const formik = useFormik({
+        initialValues: initialValues,
+        validationSchema: turnReservationSchema,
+        onSubmit: (values) => {
+            setIsLoading(true);
+            setShowAlert(false);
+            const castValues = turnReservationSchema.cast(values);
+            castValues.date = castValues.turnDate;
 
-						setResponse({
-							success: true,
-							message:
-								'Se cargaron correctamente los datos de su turno. Un administrador le enviara mas detalles de su turno por email.',
-						});
-					},
-					() => {
-						setShowAlert(true);
-						setIsLoading(false);
 
-						setResponse({
-							success: false,
-							message: 'Hubo un error al enviar el mail.',
-						});
-					}
-				);
+            privateBackendAPI
+                .post("/api/turns/turn-by-client", castValues)
+                .then(() => {
+                    formik.resetForm();
+                    setIsLoading(false);
+                    if (location?.state?.patient) {
+                        location.state = {};
+                    }
+                    setResponse({
+                        success: true,
+                        message: 'El turno se generó correctamente, se enviara un correo a la dirección ingresada',
+                    });
+                    setShowAlert(true);
+                    emailjs
+                        .sendForm(
+                            "service_pzdzjgj",
+                            "template_s8mdv36",
+                            form.current,
+                            "IA7y8FZdE1Zr4Ky_M",
+                        )
+                        .then(
+                            () => {
+                                setShowAlert(true);
+                                setIsLoading(false);
 
-			formik.resetForm();
-		},
-	});
-	return (
-		<main className='container-lg min-vh-100 d-flex flex-column justify-content-center my-3'>
-			<HeaderTitleDashboard
-				title={'Reserva tu turno'}
-				subtitle={
-					'Rellena el formulario para enviarle los datos a un administrador, este le responderá al email que proporcione sobre el estado de su turno.'
-				}
-			/>
-			<Form onSubmit={formik.handleSubmit} ref={form}>
-				<Row className='mb-lg-3 mb-1'>
-					<Col sm={12} lg={6}>
-						<h3 className='mb-lg-4 mb-3'>Datos del dueño</h3>
-						<UsersInputsForm formik={formik} editMode={true} />
-					</Col>
-					<Col sm={12} lg={6}>
-						<h3 className='mb-lg-4 mb-3'>Datos de la mascota</h3>
-						<PetInputsForm formik={formik} />
-					</Col>
-					<Col>
-						<h3 className='mb-lg-4 mb-3'>Datos del turno</h3>
-						<TurnsInputForm formik={formik} />
-					</Col>
-				</Row>
-				<CustomAlertResponse response={response} showAlert={showAlert} />
+                                setResponse({
+                                    success: true,
+                                    message:
+                                        "Se envió un email a la dirección ingresada para recordarle información acerca de su turno",
+                                });
+                            },
+                            () => {
+                                setShowAlert(true);
+                                setIsLoading(false);
 
-				<div className='d-flex justify-content-center gap-3'>
-					<Button
-						className='px-4 py-2'
-						disabled={!formik.isValid || isLoading}
-						variant={'primary'}
-						size='md'
-						type='submit'>
-						{isLoading ? (
-							<div>
-								<Spinner
-									as='span'
-									animation='border'
-									size='sm'
-									role='status'
-									aria-hidden='true'
-								/>
-								<span className='ms-2'>Cargando</span>
-							</div>
-						) : (
-							'Enviar formulario'
-						)}
-					</Button>
-					<Button
-						disabled={isLoading}
-						className='px-4 py-2'
-						variant={'danger'}
-						size='md'
-						onClick={() => {
-							formik.resetForm();
-						}}
-						type='button'>
-						Limpiar
-					</Button>
-				</div>
-			</Form>
-		</main>
-	);
+                                setResponse({
+                                    success: false,
+                                    message: "Hubo un error al enviar el mail.",
+                                });
+                            },
+                        );
+                    formik.resetForm();
+                })
+                .catch((e) => {
+                    if (e.response.data.errors) {
+                        const { errors } = e.response.data;
+                        const errorList = (
+                            <ul>
+                                {errors.map((e) => (
+                                    <li key={e.value}>{e.msg}</li>
+                                ))}
+                            </ul>
+                        );
+                        setResponse({
+                            success: false,
+                            message: errorList,
+                        });
+                        setShowAlert(true);
+                        setIsLoading(false);
+
+                        return;
+                    }
+                    // addToast({
+                    //     variant: "error",
+                    //     message: "Error al crear el turno -" + e?.response?.data?.message,
+                    // });
+                    setResponse(e?.response?.data);
+                    setShowAlert(true);
+                    setIsLoading(false);
+                });
+        },
+    });
+    return (
+        <main className="container-lg min-vh-100 d-flex flex-column justify-content-center my-3">
+            <HeaderTitleDashboard
+                title={"Reserva tu turno"}
+                subtitle={
+                    "Rellena el formulario para enviarle los datos a un administrador, este le responderá al email que proporcione sobre el estado de su turno."
+                }
+            />
+            <Form onSubmit={formik.handleSubmit} ref={form}>
+                <Row className="mb-lg-3 mb-1">
+                    <Col sm={12} lg={6}>
+                        <h3 className="mb-lg-4 mb-3">Datos del dueño</h3>
+                        <UsersInputsForm formik={formik} editMode={true} />
+                    </Col>
+                    <Col sm={12} lg={6}>
+                        <h3 className="mb-lg-4 mb-3">Datos de la mascota</h3>
+                        <PetInputsForm formik={formik} />
+                    </Col>
+                    <Col>
+                        <h3 className="mb-lg-4 mb-3">Datos del turno</h3>
+                        <TurnsInputForm formik={formik} />
+                    </Col>
+                </Row>
+                <CustomAlertResponse response={response} showAlert={showAlert} />
+
+                <div className="d-flex justify-content-center gap-3">
+                    <Button
+                        className="px-4 py-2"
+                        disabled={!formik.isValid || isLoading}
+                        variant={"primary"}
+                        size="md"
+                        type="submit"
+                    >
+                        {isLoading ? (
+                            <div>
+                                <Spinner
+                                    as="span"
+                                    animation="border"
+                                    size="sm"
+                                    role="status"
+                                    aria-hidden="true"
+                                />
+                                <span className="ms-2">Cargando</span>
+                            </div>
+                        ) : (
+                            "Enviar formulario"
+                        )}
+                    </Button>
+                    <Button
+                        disabled={isLoading}
+                        className="px-4 py-2"
+                        variant={"danger"}
+                        size="md"
+                        onClick={() => {
+                            formik.resetForm();
+                        }}
+                        type="button"
+                    >
+                        Limpiar
+                    </Button>
+                </div>
+            </Form>
+        </main>
+    );
 };
