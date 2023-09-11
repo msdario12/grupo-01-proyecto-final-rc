ae3379eb0d9e983d27ee1b2a072936bfec5a57f1
msdario12 <mansilladario94@gmail.com>
2023-09-11 17:23:13 -0300

    se cambian los regex de mascotas y turnos, se formatea codigo

--- src/dashboard/schema-validations/turnSchema.js
+++ src/dashboard/schema-validations/turnSchema.js
@@ -4,59 +4,59 @@ import { getDay, setHours } from "date-fns";
 export const vetList = ["juarez", "alvarez", "rodriguez"];
 
 export const isWeekday = (date) => {
-  const day = getDay(date);
-  return day !== 0 && day !== 6;
+    const day = getDay(date);
+    return day !== 0 && day !== 6;
 };
 
 export const filterPassedTime = (time) => {
-  const openMorning = setHours(new Date(time), 8);
-  const closeMorning = setHours(new Date(time), 12);
-  const openAfternoon = setHours(new Date(time), 16);
-  const closeAfternoon = setHours(new Date(time), 22);
-  const selectedDate = new Date(time);
+    const openMorning = setHours(new Date(time), 8);
+    const closeMorning = setHours(new Date(time), 12);
+    const openAfternoon = setHours(new Date(time), 16);
+    const closeAfternoon = setHours(new Date(time), 22);
+    const selectedDate = new Date(time);
 
-  const morningCheck =
-    selectedDate.getTime() >= openMorning.getTime() &&
-    selectedDate.getTime() < closeMorning.getTime();
-  const afternoonCheck =
-    selectedDate.getTime() >= openAfternoon.getTime() &&
-    selectedDate.getTime() < closeAfternoon.getTime();
+    const morningCheck =
+        selectedDate.getTime() >= openMorning.getTime() &&
+        selectedDate.getTime() < closeMorning.getTime();
+    const afternoonCheck =
+        selectedDate.getTime() >= openAfternoon.getTime() &&
+        selectedDate.getTime() < closeAfternoon.getTime();
 
-  return morningCheck || afternoonCheck;
+    return morningCheck || afternoonCheck;
 };
 
 export const turnEditSchema = {
-  turnDate: Yup.date()
-    .required("Campo obligatorio")
-    .test(
-      "Es dia de fin de semana",
-      "Debe ser un dia de lunes a viernes.",
-      (value) => isWeekday(value),
-    )
-    .test("Es fuera de horario", "Debe ser desde 8 a 12 ó 16 a 20hs", (value) =>
-      filterPassedTime(value),
-    ),
-  details: Yup.string()
-    .required("Campo obligatorio")
-    .min(3, "Mínimo de 3 caracteres")
-    .max(150, "Máximo de 150 caracteres")
-    .matches(/^[,.\w\-\s]+$/, "Sólo letras del alfabeto"),
-  vet: Yup.string()
-    .required("Campo obligatorio")
-    .oneOf(
-      vetList.map((vet) => vet),
-      "Selecciona una veterinario de la lista",
-    ),
+    turnDate: Yup.date()
+        .required("Campo obligatorio")
+        .test(
+            "Es dia de fin de semana",
+            "Debe ser un dia de lunes a viernes.",
+            (value) => isWeekday(value),
+        )
+        .test("Es fuera de horario", "Debe ser desde 8 a 12 ó 16 a 20hs", (value) =>
+            filterPassedTime(value),
+        ),
+    details: Yup.string()
+        .required("Campo obligatorio")
+        .min(3, "Mínimo de 3 caracteres")
+        .max(150, "Máximo de 150 caracteres")
+        .matches(/^[,.\w\-\s]+$/, "Sólo letras del alfabeto"),
+    vet: Yup.string()
+        .required("Campo obligatorio")
+        .oneOf(
+            vetList.map((vet) => vet),
+            "Selecciona una veterinario de la lista",
+        ),
 };
 
 export const turnSchema = {
-  multiSearch: Yup.string().required("Campo obligatorio"),
-  turnDate: Yup.date().test(
-    "Fecha anterior",
-    "No puede ser una fecha pasada",
-    (value) => {
-      return value > new Date();
-    },
-  ),
-  ...turnEditSchema,
+    multiSearch: Yup.string().required("Campo obligatorio"),
+    turnDate: Yup.date().test(
+        "Fecha anterior",
+        "No puede ser una fecha pasada",
+        (value) => {
+            return value > new Date();
+        },
+    ),
+    ...turnEditSchema,
 };
